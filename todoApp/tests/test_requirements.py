a715f37bf200f128cf31fdd7f3fc440f386a7523
nazarivankevych <nazar.ivankevych@gmail.com>
2024-01-02 18:30:24 +0200

    Added in unittests absolute path for requirements.txt

--- todoApp/tests/test_requirements.py
+++ todoApp/tests/test_requirements.py
@@ -1,17 +1,28 @@
+import os
 import unittest
+
 
 class TestRequirements(unittest.TestCase):
     def test_flask_present(self):
+        current_script_dir = os.path.dirname(os.path.abspath(__file__))
+        requirements_path = os.path.join(current_script_dir, "..",
+                                         "requirements.txt")
         with open('./requirements.txt') as f:
             requirements = f.read()
         self.assertIn('Flask==3.2.1', requirements)
 
     def test_sqlalchemy_present(self):
+        current_script_dir = os.path.dirname(os.path.abspath(__file__))
+        requirements_path = os.path.join(current_script_dir, "..",
+                                         "requirements.txt")
         with open('./requirements.txt') as f:
             requirements = f.read()
         self.assertIn('SQLAlchemy==2.0.24', requirements)
 
     def test_jinja_present(self):
+        current_script_dir = os.path.dirname(os.path.abspath(__file__))
+        requirements_path = os.path.join(current_script_dir, "..",
+                                         "requirements.txt")
         with open('./requirements.txt') as f:
             requirements = f.read()
         self.assertIn('Jinja2==3.1.2', requirements)
